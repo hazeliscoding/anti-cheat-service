ff80b2311ecbb46d866879f5ed613b28cdff7a33
hazeliscoding <hazel.granados@protonmail.com>
2024-10-01 15:44:53 -0500

    Finished Setup of API Project

---
+++ src/AntiCheatService.API/Controllers/PlayerActionsController.cs
@@ -0,0 +1,36 @@
+﻿using AntiCheatService.Application.Commands.RecordPlayerAction;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AntiCheatService.API.Controllers;
+
+/// <summary>
+/// Controller for handling player actions.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class PlayerActionsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerActionsController"/> class.
+    /// </summary>
+    /// <param name="mediator">The mediator.</param>
+    public PlayerActionsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Records a new player action.
+    /// </summary>
+    /// <param name="command">The command containing the action details.</param>
+    /// <returns>An asynchronous operation.</returns>
+    [HttpPost]
+    public async Task<IActionResult> RecordAction([FromBody] RecordPlayerActionCommand command)
+    {
+        await _mediator.Send(command);
+        return Ok();
+    }
+}
---
+++ src/AntiCheatService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AntiCheatService.Application.Interfaces;
+using AntiCheatService.Domain.Interfaces;
+using AntiCheatService.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntiCheatService.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IPlayerRepository, PlayerRepository>();
+    }
+}
